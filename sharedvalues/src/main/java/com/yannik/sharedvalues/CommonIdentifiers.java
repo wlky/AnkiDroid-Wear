45ef4d754e033c02dfb71da4cbdd6926c4fb3f55
Nicolas Bossard <NicolasBossard@hotmail.com>
2017-05-11 22:52:50 +0200

    Improving javadoc

--- sharedvalues/src/main/java/com/yannik/sharedvalues/CommonIdentifiers.java
+++ sharedvalues/src/main/java/com/yannik/sharedvalues/CommonIdentifiers.java
@@ -1,7 +1,7 @@
 package com.yannik.sharedvalues;
 
 /**
- * Created by Yannik on 29.04.2015.
+ * @author Created by Yannik on 29.04.2015.
  * Common string Identifiers used in the mobile and wear modules
  */
 public class CommonIdentifiers {
@@ -16,6 +16,7 @@ public class CommonIdentifiers {
 
     public static final String P2W_RESPOND_CARD = "/com.ichi2.wear/respondWithCard";
 
+    /** Message path for sending decks list. */
     public static final String P2W_COLLECTION_LIST = "/com.ichi2.wear/collections";
     public static final String W2P_EXITING = "/com.ichi2.wear/exit";
 
